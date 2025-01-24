75024a1a2f7c36105e78155d762d18d7b2922b00
rzhou10 <rzhou10@gmail.com>
2025-01-24 17:56:39 -0500

    Fixed incorrect script

--- content_scripts/block.js
+++ content_scripts/block.js
@@ -9,39 +9,19 @@
   }
   window.hasRun = true;
 
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
-  function insertBeast(beastURL) {
-    removeExistingBeasts();
-    const beastImage = document.createElement("img");
-    beastImage.setAttribute("src", beastURL);
-    beastImage.style.height = "100vh";
-    beastImage.className = "beastify-image";
-    document.body.appendChild(beastImage);
-  }
 
-  /**
-   * Remove every beast from the page.
-   */
-  function removeExistingBeasts() {
-    const existingBeasts = document.querySelectorAll(".beastify-image");
-    for (const beast of existingBeasts) {
-      beast.remove();
-    }
-  }
-
-  /**
-   * Listen for messages from the background script.
-   * Call "insertBeast()" or "removeExistingBeasts()".
-   */
   browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "beastify") {
-      insertBeast(message.beastURL);
-    } else if (message.command === "reset") {
-      removeExistingBeasts();
+    if (message.command === "urlToBlock") {
+      urls.push(message.websiteUrl);
+    } else if (message.command === "block-url") {
+      // block the element on load
+      if (urls.include()) {
+        const body = document.getElementsByTagName("body");
+        body.replaceChildren();
+        const div = document.createElement('div');
+        div.innerHTML = "Please get a life"
+        body.appendChild(div);
+      }
     }
   });
 })();
